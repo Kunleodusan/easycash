9ca10d11de299bf8861dd6aa26acf668e379f250
Kunleodusan <odusanayokunle@gmail.com>
2017-03-25 10:41:18 +0100

    try to fix CORS 6

--- api/app/Http/Middleware/CORS2.php
+++ api/app/Http/Middleware/CORS2.php
@@ -15,7 +15,8 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin' , '*')
+        return $next($request)
+            ->header('Access-Control-Allow-Origin' , '*')
             ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
     }
