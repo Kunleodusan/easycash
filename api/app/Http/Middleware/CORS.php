9f9853b734c4c03ecb981068e6d04f412de54f33
Kunleodusan <odusanayokunle@gmail.com>
2017-03-25 11:14:58 +0100

    try to fix CORS 11

--- api/app/Http/Middleware/CORS.php
+++ api/app/Http/Middleware/CORS.php
@@ -16,7 +16,7 @@ class CORS
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin' , 'http://docs.recodenigeria.tk')
+            ->header('Access-Control-Allow-Origin' , 'http://docs.recodenigeria.tk,http://app.recodenigeria.tk')
             ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With,Access-Token');
     }
