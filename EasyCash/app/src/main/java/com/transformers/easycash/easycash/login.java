c52cfcbd3a6cf662a78e5f854828574dfb54f559
Benjineer <oke3ben@gmail.com>
2017-03-26 07:29:33 +0100

    first sunday android commit

--- EasyCash/app/src/main/java/com/transformers/easycash/easycash/login.java
+++ EasyCash/app/src/main/java/com/transformers/easycash/easycash/login.java
@@ -53,29 +53,35 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
 //                Intent it = new Intent(login.this, signup.class);
 //                startActivity(it);
-                String url = "http://api.recodenigeria.tk/api/v1/customer/login";
-                HashMap<String, String> params = new HashMap<String, String>();
-                params.put("email", mail.getText().toString());
-                params.put("password", pswd.getText().toString());
-                JSONObject jsonBody = new JSONObject(params);
-                boolean status = false;
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.i(RESPONSELOG, response.toString());
-                        Intent it = new Intent(login.this, DashBoardActivity.class);
-                        startActivity(it);
+                if (mail.getText().toString().isEmpty() || mail.getText().toString() == null) {
+                    mail.setError("Email is required");
+                } else if (pswd.getText().toString().isEmpty() || pswd.getText().toString() == null) {
+                    pswd.setError("Password is required");
+                } else {
 
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i(RESPONSELOG, error.toString());
-                        Snackbar.make(findViewById(R.id.loginRelativeLayout), "Invalid email or password", Snackbar.LENGTH_SHORT).show();
-                    }
-                });
-                queue.add(jsonObjectRequest);
+                    String url = "http://api.recodenigeria.tk/api/v1/customer/login";
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    params.put("email", mail.getText().toString());
+                    params.put("password", pswd.getText().toString());
+                    JSONObject jsonBody = new JSONObject(params);
+                    boolean status = false;
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.i(RESPONSELOG, response.toString());
+                            Intent it = new Intent(login.this, DashBoardActivity.class);
+                            startActivity(it);
 
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i(RESPONSELOG, error.toString());
+                            Snackbar.make(findViewById(R.id.loginCoordLayout), "Invalid email or password", Snackbar.LENGTH_SHORT).show();
+                        }
+                    });
+                    queue.add(jsonObjectRequest);
+                }
             }
         });
 
